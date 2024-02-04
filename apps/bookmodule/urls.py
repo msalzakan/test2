9ced51da29c4aee6b77037746de5767610aef90a
User Test <msalzakan@live.com>
2024-02-04 08:35:01 +0300

    t1

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -21,5 +21,5 @@ from . import views
 urlpatterns = [
     #path("admin/", admin.site.urls),
     path('', views.index, name='index'),
-     path('members/', views.members, name='members'),
+     path('members0/', views.members, name='members'),
 ]
